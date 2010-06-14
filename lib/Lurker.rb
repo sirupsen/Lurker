adcf1400bf718c33c3ddb68c0d06a358040ca407
Noxn Xon <noxn@gmx.com>
2010-06-14 20:38:05 +0100

    Finally, mongo.

--- lib/Lurker.rb
+++ lib/Lurker.rb
@@ -1,4 +1,8 @@
+require 'rubygems'
 require 'socket'
+require 'mongo'
+require 'time'
+
 class Lurker
   def initialize(channel, server, port, name)
     #Make variables we will use.
@@ -8,7 +12,11 @@ class Lurker
   def connect
     #Create socket, send ident, name and other stuff.
     #Connect to channel.
+    @db = Mongo::Connection.new("localhost").db("lurker")
+    raise "Cant connect to MongoDB, turn that on first, stupid." unless @db
+    @coll = @db.collection("#{@server.gsub(".", "_")}__#{@channel.gsub(".", "_")}")
     @s = TCPSocket.open(@server, @port)
+    raise "Cant connect to IRC server." unless @s
     @s.send "USER Lurker durr derp :no wai \n", 0
     @s.send "NICK #{@name} \n", 0
     @s.send "JOIN ##{@channel} \n", 0
@@ -20,13 +28,16 @@ class Lurker
     while @work
       #Socket receive and store.
       #Respond to pings.
-      line = @s.gets
-      p line
+      line = @s.gets #Receive.
+      p line  #For debug purposes.
       line = line.chomp.split(" ")
 
       if line[1] == "PRIVMSG"
         guy = line[0].split("!")[0]
-        p "#{guy} #{line[3..-1].join(" ")}"
+        msg = line[3..-1].join(" ")
+        p "#{guy} #{line[3..-1].join(" ")}" #For debug purposes.
+        doc = {"name" => guy, "message" => msg, "date" => Time.now.to_i}
+        @coll.insert(doc)
       elsif line[0] == "PING"
         @s.send "PONG #{line[1]}", 0
         p "PONG #{line[1]}"
